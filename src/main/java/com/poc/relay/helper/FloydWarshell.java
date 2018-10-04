c3a939458033aa61a67c78ebf070f1cdd62efd59
SVD Prasad <svd@fptechscience.com>
2018-10-05 00:19:12 +0530

    shortest path

---
+++ src/main/java/com/poc/relay/helper/FloydWarshell.java
@@ -0,0 +1,97 @@
+package com.poc.relay.helper;
+
+/**
+ * @author svdprasad
+ */
+public class FloydWarshell
+{
+    // Recursive Function to print path of given
+    // vertex u from source vertex v
+    private void printPath(int[][] path, int v, int u)
+    {
+        if (path[v][u] == v)
+            return;
+
+        printPath(path, v, path[v][u]);
+        System.out.print(path[v][u] + " ");
+    }
+
+    // Function to print the shortest cost with path
+    // information between all pairs of vertices
+    private void printSolution(int[][] cost, int[][] path, int N)
+    {
+        for (int v = 0; v < N; v++)
+        {
+            for (int u = 0; u < N; u++)
+            {
+                if (u != v && path[v][u] != -1)
+                {
+                    System.out.print("Shortest Path from vertex " + v +
+                            " to vertex " + u + " is (" + v + " ");
+                    printPath(path, v, u);
+                    System.out.println(u + ")");
+                }
+            }
+        }
+    }
+
+    // Function to run Floyd-Warshell algorithm
+    public void floydWarshell(int[][] adjMatrix, int N)
+    {
+        // cost[] and parent[] stores shortest-path
+        // (shortest-cost/shortest route) information
+        int[][] cost = new int[N][N];
+        int[][] path = new int[N][N];
+
+        // initialize cost[] and parent[]
+        for (int v = 0; v < N; v++)
+        {
+            for (int u = 0; u < N; u++)
+            {
+                // initally cost would be same as weight
+                // of the edge
+                cost[v][u] = adjMatrix[v][u];
+
+                if (v == u)
+                    path[v][u] = 0;
+                else if (cost[v][u] != Integer.MAX_VALUE)
+                    path[v][u] = v;
+                else
+                    path[v][u] = -1;
+            }
+        }
+
+        // run Floyd-Warshell
+        for (int k = 0; k < N; k++)
+        {
+            for (int v = 0; v < N; v++)
+            {
+                for (int u = 0; u < N; u++)
+                {
+                    // If vertex k is on the shortest path from v to u,
+                    // then update the value of cost[v][u], path[v][u]
+
+                    if (cost[v][k] != Integer.MAX_VALUE
+                            && cost[k][u] != Integer.MAX_VALUE
+                            && (cost[v][k] + cost[k][u] < cost[v][u]))
+                    {
+                        cost[v][u] = cost[v][k] + cost[k][u];
+                        path[v][u] = path[k][u];
+                    }
+                }
+
+                // if diagonal elements become negative, the
+                // graph contains a negative weight cycle
+                if (cost[v][v] < 0)
+                {
+                    System.out.println("Negative Weight Cycle Found!!");
+                    return;
+                }
+            }
+        }
+
+        // Print the shortest path between all pairs of vertices
+        printSolution(cost, path, N);
+    }
+}
+
--- src/main/java/com/poc/relay/processor/ComputeWaitTime.java
+++ src/main/java/com/poc/relay/processor/ComputeWaitTime.java
@@ -1,5 +1,6 @@
 package com.poc.relay.processor;
 
+import com.poc.relay.helper.FloydWarshell;
 import com.poc.relay.models.Pilot;
 import com.poc.relay.models.PitStop;
 import com.poc.relay.models.Schedule;
@@ -15,7 +16,8 @@ import java.util.Queue;
  */
 public class ComputeWaitTime {
 
-    final static int INF = 99999, V = 3;
+    final int  V = 4;
+    int M = Integer.MAX_VALUE;
     int dist[][] = new int[V][V];
 
     //Initialize pitStop queues for Left and Right directions
@@ -24,10 +26,13 @@ public class ComputeWaitTime {
     PitStop pitStopC = new PitStop("B", "A", "C");
 
 
-    int graph[][] = { {0, 1,  1},
-            {1, 0, 1},
-            {1, 1, 0}
-    };
+    int graph[][] =  new int[][]
+            {
+                    { 0,  M, -2, M },
+                    { 4,  0,  3, M },
+                    { M,  M,  0, 2 },
+                    { M, -1,  M, 0 }
+            };
 
 
     Schedule schedule1 = new Schedule(pitStopA, pitStopC, pitStopA,111, 22, 48);
@@ -50,10 +55,9 @@ public class ComputeWaitTime {
         pitStopA.getLeftPitStopQueue().add(pilot1);
         pitStopA.getRightPitStopQueue().add(pilot2);
 
+        FloydWarshell floydWarshell = new FloydWarshell();
+        floydWarshell.floydWarshell(graph, V);
 
-
-        AllPairShortestPath a = new AllPairShortestPath();
-        a.floydWarshall(graph);
 
         for(Schedule schedule: schedulePriorityQueue) {
 
@@ -61,12 +65,6 @@ public class ComputeWaitTime {
 
 
         }
-
-
-
-
-
-
 
 
     }
@@ -86,71 +84,7 @@ public class ComputeWaitTime {
     }
 
 
-    class AllPairShortestPath
-    {
 
-
-        void floydWarshall(int graph[][])
-        {
-
-            int i, j, k;
-
-        /* Initialize the solution matrix same as input graph matrix.
-           Or we can say the initial values of shortest distances
-           are based on shortest paths considering no intermediate
-           vertex. */
-            for (i = 0; i < V; i++)
-                for (j = 0; j < V; j++)
-                    dist[i][j] = graph[i][j];
-
-        /* Add all vertices one by one to the set of intermediate
-           vertices.
-          ---> Before start of an iteration, we have shortest
-               distances between all pairs of vertices such that
-               the shortest distances consider only the vertices in
-               set {0, 1, 2, .. k-1} as intermediate vertices.
-          ----> After the end of an iteration, vertex no. k is added
-                to the set of intermediate vertices and the set
-                becomes {0, 1, 2, .. k} */
-            for (k = 0; k < V; k++)
-            {
-                // Pick all vertices as source one by one
-                for (i = 0; i < V; i++)
-                {
-                    // Pick all vertices as destination for the
-                    // above picked source
-                    for (j = 0; j < V; j++)
-                    {
-                        // If vertex k is on the shortest path from
-                        // i to j, then update the value of dist[i][j]
-                        if (dist[i][k] + dist[k][j] < dist[i][j])
-                            dist[i][j] = dist[i][k] + dist[k][j];
-                    }
-                }
-            }
-
-            // Print the shortest distance matrix
-            printSolution(dist);
-        }
-
-        void printSolution(int dist[][])
-        {
-            System.out.println("The following matrix shows the shortest "+
-                    "distances between every pair of vertices");
-            for (int i=0; i<V; ++i)
-            {
-                for (int j=0; j<V; ++j)
-                {
-                    if (dist[i][j]==INF)
-                        System.out.print("INF ");
-                    else
-                        System.out.print(dist[i][j]+"   ");
-                }
-                System.out.println();
-            }
-        }
-
-    }
     public void print() {
         System.out.println(schedule1.toString());
     }
